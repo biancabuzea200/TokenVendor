46d6875d067adeb486f3a7e66ce2dbfe351f2f7c
biancabuzea200 <biancabuzea200@gmail.com>
2024-06-29 20:25:06 +0200

    Add comments Token.sol

--- TokenVendor/src/Token.sol
+++ TokenVendor/src/Token.sol
@@ -4,16 +4,17 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
 
 contract Token is ERC20, AccessControl {
+    // Create a new role identifier for the minter role
 	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-	constructor(address defaultAdmin, address minter)
-    	ERC20("BiancaToken", "BIA")
+	constructor(address defaultAdmin, address minter) ERC20("BiancaToken", "BIA")
   	 
 	{
     	_grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        // Grant the minter role to a specified account
     	_grantRole(MINTER_ROLE, minter);
 	}
 
