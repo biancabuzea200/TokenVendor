6d0fece529c545f44830bc64c75349d9ecf2292c
biancabuzea200 <biancabuzea200@gmail.com>
2024-07-03 13:57:01 +0200

    Add deployment scripts wip

--- script/deploy_token.s.sol
+++ script/deploy_token.s.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/TokenVendor.sol";
+import "../src/Token.sol";
 
 contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployerPublicKey = vm.envAddress("PUBLIC_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        TokenVendor tokenVendor = new TokenVendor(address('0x9C789164dc8b39B4B05FF6b161c62636589C87E6'));
+        Token token = new Token(deployerPublicKey, deployerPublicKey);
 
         vm.stopBroadcast();
     }
